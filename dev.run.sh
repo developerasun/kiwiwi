aafbc35e629d7bd1dd77307288d46f87bffd2dd1
developerasun <nellow1102@gmail.com>
2026-01-10 01:05:55 +0900

    add flag display helper

--- dev.run.sh
+++ dev.run.sh
@@ -4,32 +4,33 @@ set -e
 FLAG="$1"
 FILENAME="$2"
 
-# 1. always run application
+listAvailableFlags() {
+  echo "❌ Unknown FLAG: $FLAG"
+  echo "Supported flags:"
+  echo "  ./dev.run.sh         # build and run whole application"
+  echo "  ./dev.run.sh --build-one [filename]  # build and run one target file"
+  echo "  ./dev.run.sh --test   # run test suites"
+}
+
 echo "▶ Kiwiwi app start"
 zig build run
 
-# 2. 인자 없으면 여기서 종료
 if [ -z "$FLAG" ]; then
   echo "✔ App running only (no apply)"
   exit 0
 fi
 
-# 3. 환경별 apply
 case "$FLAG" in
   --test)
     echo "▶ Running test cases"
     zig build test --verbose
     ;;
-  --one)
+  --build-one)
     echo "▶ Build and run one target file"
     zig run $FILENAME
     ;;
   *)
-    echo "❌ Unknown FLAG: $FLAG"
-    echo "Usage:"
-    echo "  ./dev.run.sh         # build and run whole application"
-    echo "  ./dev.run.sh --single [filename]  # build and run one target file"
-    echo "  ./dev.run.sh --test   # run test suites"
+    listAvailableFlags
     exit 1
     ;;
 esac
