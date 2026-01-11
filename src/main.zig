a38910a9f4336b000d901ef35cb4652e2e35059f
developerasun <nellow1102@gmail.com>
2026-01-11 20:56:06 +0900

    refactor main program error handling

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
 const kiwiwi = @import("kiwiwi");
 
+// @dev catch all elevated errors and terminate the app if exists.
 pub fn main() !void {
-    kiwiwi.metadata();
-    const controllerName = try kiwiwi.ParseArgument();
-    try kiwiwi.Generate(controllerName);
+    std.debug.print("project {s} with version {d}\n", .{ "kiwiwi", 0.1 });
+
+    kiwiwi.Run() catch |err| {
+        std.debug.print("Failed to run Kiwiwi application with reason: {s}, terminating app.\n", .{@errorName(err)});
+        std.process.exit(1);
+    };
 }
