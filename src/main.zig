60e815f262dd56cf67bfd9e3ee48afb3902c9693
developerasun <nellow1102@gmail.com>
2026-01-23 18:30:41 +0900

    improve ux with logging color

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,9 @@ const kiwiwi = @import("kiwiwi");
 // @dev catch all elevated errors and terminate the app if exists.
 pub fn main() !void {
     kiwiwi.Run() catch |err| {
-        std.debug.print("Failed to run with reason: {s}, terminating app.\n", .{@errorName(err)});
+        const red = "\x1b[31m";
+        const reset = "\x1b[0m";
+        std.debug.print("\n{s}[X] {s}{s} Kiwiwi crashed, terminating app.\n", .{ red, @errorName(err), reset });
         std.process.exit(1);
     };
 }
